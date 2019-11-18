2964db1e9420a5f851264bbda0f909ae2d28e8d1
Ramachandra Chakenalli Nanjegowda <cn.ramachandra@gmail.com>
2019-11-18 01:25:32 -0700

    flappie build integration

--- gemx/src/flappie_test.cpp
+++ gemx/src/flappie_test.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <math.h>
 #include "gemx_xblas.h"
+#include <vector>
+using namespace std;
 
 int max(int x, int y){
    if(x > y) return x;
@@ -12,27 +14,37 @@ int max(int x, int y){
 
 int main(void)
 {
-    const int m = 4;
+    const int m = 8;
     const int n = 8;
-    const int k = 1;
+    const int k = 8;
 
-    short A[] = {1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8};
-    short B[] = { 1, 2, 3, 4 };
+    short A[] = {1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8, 1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8};
+    short B[] = {1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8, 1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8,   1,2,3,4,5,6,7,8};
 
 
-      std::vector<GEMX_dataType> a1(m*n);
-      std::vector<GEMX_dataType> b1(m*1);
-      std::vector<GEMX_dataType> c1(n*1);
+    std::vector<GEMX_dataType> a1(m*n);
+    std::vector<GEMX_dataType> b1(m*k);
+    std::vector<GEMX_dataType> c1(n*k);
 
-    //memcpy(a1, A, m * n * sizeof(short));
-    //memcpy(b1, B, m * 1 * sizeof(short));
-    //memset(c1, 0, n * 1 * sizeof(short));
+    //memcpy(&a1[0], A, m*n*sizeof(short));
+    //memcpy(&b1[0], B, m*n*sizeof(short));
+  
+      for (int row = 0; row < m;  ++row) {
+            for (int col = 0; col < n;  ++col) {
+              a1[row*m+col] = A[row*m+col];
+              b1[row*m+col] = B[row*m+col];
+              c1[row*m+col] = 0;
+            }
+          }
 
     //cblas_sgemm(CblasColMajor, CblasTrans, CblasNoTrans, n,k,m, alpha, a1, m, b1, n, beta, c1, n);
-    xblas_sgemm(CblasRowMajor,CblasTrans,CblasNoTrans,n,k,m,1,a1,m,b1,n,1,c1,n);
+    xblas_sgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,n,k,m,1,a1,m,b1,n,1,c1,n);
 
 
     fprintf(stderr,"Result after sgemm\n"); 
+    for (int i = 0; i < c1.size(); i++)
+        printf("%d ",c1[i]);
+
 
     clearMemory();
 
